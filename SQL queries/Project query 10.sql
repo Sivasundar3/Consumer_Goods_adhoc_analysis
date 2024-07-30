01bc7756d5aa641f754a1c7b6a6801c346e1313d
SIVASUNDAR T P <121023707+Sivasundar3@users.noreply.github.com>
2024-07-30 18:41:00 +0530

    Update Project query  10

--- SQL queries/Project query 10.sql
+++ SQL queries/Project query 10.sql
@@ -1,10 +1,10 @@
 /* 10 .Get the Top 3 products in each division that have a high total_sold_quantity in the fiscal_year 2021? 
        The final output contains these fields,
-											 division 
-											 product_code
-											 product 
-											 total_sold_quantity
-											 rank_order                      */
+	                                       division 
+                                               product_code
+					       product 
+					       total_sold_quantity
+					       rank_order                      */
 
 
 WITH  sold_quantity AS
