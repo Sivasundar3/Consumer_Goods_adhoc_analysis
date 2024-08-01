d90d1a7ad3b0c3fb96600a9009cd4764babbee3f
SIVASUNDAR T P <121023707+Sivasundar3@users.noreply.github.com>
2024-08-01 09:10:36 +0530

    Update Project query 3

--- SQL queries/Project query 3.sql
+++ SQL queries/Project query 3.sql
@@ -1,8 +1,8 @@
-/* 3.Provide a report with all the unique product counts for each segment 
-	 and sort them in descending order of product counts. 
+/* 3.Provide a report with all the unique product counts for each segment and sort them in descending order of product counts. 
      The final output contains 2 fields, 
-	 segment 
-     product_count */
+	                                  segment 
+                                          product_count    */
+
      
 SELECT segment,COUNT(product_code) as product_count
 FROM gdb023.dim_product
