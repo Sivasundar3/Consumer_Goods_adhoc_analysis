d1b7e33982c66b577f4997f41edbe15185f0274b
SIVASUNDAR T P <121023707+Sivasundar3@users.noreply.github.com>
2024-08-01 09:15:50 +0530

    Update Project query 7

--- SQL queries/Project query 7.sql
+++ SQL queries/Project query 7.sql
@@ -1,9 +1,9 @@
-/* 7 . Get the complete report of the Gross sales amount for the customer “Atliq Exclusive” for each month. This analysis helps to get an idea of low and high-performing months and take strategic decisions. 
+/* 7 . Get the complete report of the Gross sales amount for the customer “Atliq Exclusive” for each month. 
+       This analysis helps to get an idea of low and high-performing months and take strategic decisions. 
        The final report contains these columns:
-
-									           Month 
-											   Year
-									           Gross sales Amount    */
+                                                Month 
+					        Year
+						Gross sales Amount    */
      
 
 
