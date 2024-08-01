d2f14bb54ec5d79c5701686685bd9a9e79934402
SIVASUNDAR T P <121023707+Sivasundar3@users.noreply.github.com>
2024-08-01 09:17:33 +0530

    Update Project query 9

--- SQL queries/Project query 9.sql
+++ SQL queries/Project query 9.sql
@@ -1,8 +1,8 @@
-/* 9.Which channel helped to bring more gross sales in the fiscal year 2021 and the percentage of contribution? 
-     The final output contains these fields, 
-										    channel ,
-											gross_sales_mln,
-										    percentage           */
+/* 9. Which channel helped to bring more gross sales in the fiscal year 2021 and the percentage of contribution? 
+      The final output contains these fields, 
+					    channel 
+					    gross_sales_mln
+					    percentage           */
 
 
 
